5a5dca769a5a9e67ec4459dbd9f62409037e7dc3
koushik4455 <gaddamkoushik480@gmail.com>
2024-01-24 17:54:21 +0530

    adding fruits

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@
 
 import { UseStateExample1 } from './components/useState/useStateEx1';
 import { UseSateExample2 } from './components/useState/useStateEx2';
+import { UseStateExample3 } from './components/useState/useStateEx3';
 
 function App() {
   return (
     <>
-   <UseStateExample1/> 
-   <UseSateExample2/>
+   {/* <UseStateExample1/> 
+   <UseSateExample2/> */}
+   <UseStateExample3/>
   
   {/* <CircleApp/> */}
    </>
--- src/components/useState/useStateEx3.js
+++ src/components/useState/useStateEx3.js
@@ -1,5 +1,29 @@
+import React, { useState } from "react"
 
 
 
 
 
+export const UseStateExample3=()=>{
+    const initialvalue=["apple","mango"]
+    const [list,setList]=useState(initialvalue)
+
+    const handleFruit=()=>{
+        const fruitname="kiwi"
+        setList([...list,fruitname])
+
+    }
+
+
+    return(
+        <>
+            <button onClick={handleFruit}>click to add new fruit</button>
+            <ol>
+                {list.map((value,index)=><React.Fragment key={index}>
+
+                    <li>{value}</li>
+                </React.Fragment>)}
+            </ol>
+        </>
+    )
+}
